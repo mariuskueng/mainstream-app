c6b45c0ceea05dec6788fad5dec076cc9a14bb02
Marius Küng <maku@mailbox.org>
2016-04-06 17:25:36 +0200

    Added concert fixtures to Concert class
    Removed date property. Using date as dictionary key.

--- mainstream-app/Concert.swift
+++ mainstream-app/Concert.swift
@@ -9,15 +9,34 @@
 import Foundation
 
 class Concert {
-    var date: String
+//    var date: String
     var artist: String
     var venue: String
     var city: String
     
-    init(date: String, artist: String, venue: String, city: String) {
-        self.date = date
+    init(artist: String, venue: String, city: String) {
         self.artist = artist
         self.venue = venue
         self.city = city
+    }
+    
+    static func getConcerts () -> [String: [Concert]] {
+        return [
+            "01.04.2016": [
+                Concert(artist: "Secret Sight / Aie Ça Gicle", venue: "Hirscheneck", city: "Basel"),
+                Concert(artist: "The Holydrug Couple / Moreeats", venue: "Treppenhaus", city: "Rorschach"),
+                Concert(artist: "The Ar-Kaics / James & The Ultrasounds", venue: "Horst", city: "Kreuzlingen")
+            ],
+            "02.04.2016": [
+                Concert(artist: "Gangstarr Foundation feat. Jeru The Damaja, Afu Ra & Big Shug", venue: "Kaserne", city: "Basel"),
+                Concert(artist: "Louis Barabbas & The Bedlam Six", venue: "Parterre", city: "Basel"),
+                Concert(artist: "DIIV / Ulrika Spacek", venue: "Mascotte", city: "Zürich")
+            ],
+            "03.04.2016": [
+                Concert(artist: "Farao / Dralms", venue: "1. Stock", city: "Münchenstein"),
+                Concert(artist: "Louis Barabbas & The Bedlam Six", venue: "El Lokal", city: "Zürich"),
+                Concert(artist: "Pere Ubu", venue: "Sedel", city: "Luzern")
+            ]
+        ]
     }
 }
