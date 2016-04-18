e9e54b0a66e4993001b39c7897a16f073096e4b5
Marius Küng <maku@mailbox.org>
2016-04-18 21:26:56 +0200

    Updated playground

--- mainstream.playground/Contents.swift
+++ mainstream.playground/Contents.swift
@@ -2,17 +2,6 @@
 
 import UIKit
 
-var str = "Hello, playground"
-
-var today = NSDate()
-
-var dateAsString = "24-12-2015 23:59"
-
-let dateFormatter = NSDateFormatter()
-dateFormatter.dateFormat = "dd-MM-yyyy HH:mm"
-var newDate = dateFormatter.dateFromString(dateAsString)
-
-let anotherDateFormatter = NSDateFormatter()
-anotherDateFormatter.locale = NSLocale(localeIdentifier: "de_DE")
-anotherDateFormatter.dateFormat = "dd.MM.YYYY"
-var anotherDate = anotherDateFormatter.dateFromString("24.12.2015")
+var formatter = NSDateFormatter()
+formatter.dateFormat = "dd.MM.yyyy"
+let dateTimePrefix: String = formatter.stringFromDate(NSDate(timeIntervalSince1970: 1473804000))
