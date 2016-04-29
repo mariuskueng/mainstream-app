1d25dad7a0bfe52eda9df25b32f30ad347c35bd3
Marius Küng <maku@mailbox.org>
2016-04-29 13:20:33 +0200

    Added textfield & datepicker
    Added action on date changed

--- mainstream-app/ConcertTableViewController.swift
+++ mainstream-app/ConcertTableViewController.swift
@@ -18,14 +18,16 @@ class ConcertTableViewController: UITableViewController {
     }
     
     var concertDict = [String: [Concert]]()
+    var concertsStorage = [TableViewObjects]()
     var concerts = [TableViewObjects]()
     var cities = []
     var dates = []
     let dateFormatter = NSDateFormatter()
     var apiUrl = "https://arcane-hollows-16881.herokuapp.com"
+    var selectedDate = NSDate()
     
     @IBOutlet weak var cityButton: UIButton!
-    @IBOutlet weak var dateButton: UIButton!
+    @IBOutlet weak var dateTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -62,11 +64,13 @@ class ConcertTableViewController: UITableViewController {
                 
                 // convert dict to array
                 for (key, value) in self.concertDict {
-                    self.concerts.append(TableViewObjects(sectionName: key, sectionObjects: value))
+                    self.concertsStorage.append(TableViewObjects(sectionName: key, sectionObjects: value))
                 }
                 
                 // sort array after date ascending
-                self.concerts.sortInPlace({$0.sectionName < $1.sectionName})
+                self.concertsStorage.sortInPlace({$0.sectionName < $1.sectionName})
+                
+                self.concerts = self.concertsStorage
                 
                 // reload table view to add data
                 self.tableView.reloadData()
@@ -82,8 +86,56 @@ class ConcertTableViewController: UITableViewController {
         self.cities = concerts.map{ c in c.city }
         self.dates = Array(self.concertDict.keys).flatMap{$0}
     }
+    
+    @IBAction func textFieldEditing(sender: UITextField) {
+        //Create the view
+        let inputView = UIView(frame: CGRectMake(0, 0, self.view.frame.width, 240))
+        
+        let datePickerView  : UIDatePicker = UIDatePicker(frame: CGRectMake(0, 40, 0, 0))
+        datePickerView.datePickerMode = UIDatePickerMode.Date
+        inputView.addSubview(datePickerView) // add date picker to UIView
+        
+        let doneButton = UIButton(frame: CGRectMake((self.view.frame.size.width - 100), 0, 100, 50))
+        doneButton.setTitle("Done", forState: UIControlState.Normal)
+        doneButton.setTitle("Done", forState: UIControlState.Highlighted)
+        doneButton.setTitleColor(UIColor.blueColor(), forState: UIControlState.Normal)
+        doneButton.setTitleColor(UIColor.grayColor(), forState: UIControlState.Highlighted)
+        
+        inputView.addSubview(doneButton) // add Button to UIView
+        
+        doneButton.addTarget(self, action: #selector(ConcertTableViewController.doneButtonClicked), forControlEvents: UIControlEvents.TouchUpInside) // set button click event
+        
+        sender.inputView = inputView
+        datePickerView.addTarget(self, action: #selector(ConcertTableViewController.datePickerValueChanged), forControlEvents: UIControlEvents.ValueChanged)
+        
+        datePickerValueChanged(datePickerView)
     }
-
+    
+    func datePickerValueChanged(sender: UIDatePicker) {
+        self.dateTextField.text = getFormattedDate(sender.date)
+        print("Date picker value changed")
+        self.selectedDate = sender.date
+    }
+    
+    func doneButtonClicked(sender:UIButton)
+    {
+        print("Done button clicked")
+//        filterByDate()
+        self.dateTextField.text = getFormattedDate(self.selectedDate)
+        self.dateTextField.resignFirstResponder() // To resign the inputView on clicking done.
+    }
+    
+    func getFormattedDate(date: NSDate) -> String {
+        return self.dateFormatter.stringFromDate(date)
+    }
+    
+    func filterByDate() {
+        self.concerts = self.concerts.filter{$0.sectionName == getFormattedDate(self.selectedDate)}
+        
+        
+        self.tableView.reloadData()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
