df92716f1a906f837cc1fbd7e7843d59c835cb7b
Marius Küng <maku@mailbox.org>
2016-05-06 10:26:47 +0200

    Updated playground

--- mainstream.playground/Contents.swift
+++ mainstream.playground/Contents.swift
@@ -3,5 +3,10 @@
 import UIKit
 
 var formatter = NSDateFormatter()
-formatter.dateFormat = "dd.MM.yyyy"
+formatter.dateFormat = "dd.MM.YYYY"
 let dateTimePrefix: String = formatter.stringFromDate(NSDate(timeIntervalSince1970: 1473804000))
+
+let dateFormatter = NSDateFormatter()
+dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+let anotherDate = dateFormatter.dateFromString("2016-04-26T22:00:00.000Z")
+
